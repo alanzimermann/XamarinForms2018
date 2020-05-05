d21fc237fc25b49d783dc04c1786c6d280525698
Alan Zimermann <alanzimermann@hotmail.com>
2020-05-05 20:17:48 -0300

    Secao 08

---
+++ Secao 08/App2_Tarefa/App2_Tarefa/App2_Tarefa/Telas/Cadastro.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using App2_Tarefa.Modelos;
+
+namespace App2_Tarefa.Telas
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Cadastro : ContentPage
+    {
+        private byte Prioridade { get; set; }
+
+        public Cadastro()
+        {
+            InitializeComponent();
+        }
+
+        private void PrioridadeSelectionAction(object sender, EventArgs args)
+        {
+            var Stacks = SLPrioridades.Children;
+
+            foreach (var Linha in Stacks)
+            {
+                Label LblPrioridade = ((StackLayout)Linha).Children[1] as Label;
+                LblPrioridade.TextColor = Color.Gray;
+            }
+
+            ((Label)((StackLayout)sender).Children[1]).TextColor = Color.Black;
+            FileImageSource Source = ((Image)((StackLayout)sender).Children[0]).Source as FileImageSource;
+
+            String Prioridade = Source.File.ToString().Replace("Resources/", "").Replace(".png", "").Replace("p", "");
+            this.Prioridade = byte.Parse(Prioridade);
+        }
+
+        private void SalvarAction(object sender, EventArgs args)
+        {
+            bool ErroExiste = false;
+
+            //Verificando se os dois campos estão preenchidos. Para strings, verificando se o tamanho é maior que 0:
+            if (TxtNome.Text == null || TxtNome.Text.Trim().Length <= 0)
+            {
+                ErroExiste = true;
+                DisplayAlert("Erro", "Tarefa não digitada", "Okay");
+            }
+
+            //Já, para a prioridade, 
+            if (Prioridade <= 0)
+            {
+                ErroExiste = true;
+                DisplayAlert("Erro", "Prioridade não escolhida", "Okay");
+            }
+
+            if (ErroExiste == false)
+            {
+                Tarefa tarefa = new Tarefa();
+                tarefa.Nome = TxtNome.Text.Trim();
+                tarefa.Prioridade = this.Prioridade;
+
+                new GerenciadorTarefa().Salvar(tarefa);
+
+                App.Current.MainPage = new NavigationPage(new Inicio());
+            }
+        }
+    }
+}
