a9cb335803f356c92dbe1c8fda7e48fa698e71f0
Alan Zimermann <alanzimermann@hotmail.com>
2020-05-12 18:34:53 -0300

    Secao 13

---
+++ Secao 13/App1_NossoChat/App1_NossoChat/App1_NossoChat/Util/UsuarioUtil.cs
@@ -0,0 +1,28 @@
+﻿using App1_NossoChat.Model;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App1_NossoChat.Util
+{
+    public class UsuarioUtil
+    {
+        public static void SetUsuarioLogado(Usuario usuario)
+        {
+            App.Current.Properties["LOGIN"] = JsonConvert.SerializeObject(usuario);
+        }
+
+        public static Usuario GetUsuarioLogado()
+        {
+            if (App.Current.Properties.ContainsKey("LOGIN"))
+            {
+                return JsonConvert.DeserializeObject<Usuario>((string)App.Current.Properties["LOGIN"]);
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
---
+++ Secao 13/App1_NossoChat/App1_NossoChat/App1_NossoChat/ViewModel/ChatsViewModel.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using App1_NossoChat.Model;
+using App1_NossoChat.Service;
+using Xamarin.Forms;
+
+namespace App1_NossoChat.ViewModel
+{
+    public class ChatsViewModel : INotifyPropertyChanged
+    {
+        private Chat _SelectedItemChat;
+        public Chat SelectedItemChat
+        {
+            get { return _SelectedItemChat; }
+            set
+            {
+                _SelectedItemChat = value;
+                OnPropertyChanged("SelectedItemChat");
+                GoPaginaMensagem(value);
+            }
+        }
+
+        private void GoPaginaMensagem(Chat chat)
+        {
+            if (chat != null)
+            {
+                SelectedItemChat = null;
+                ((NavigationPage)App.Current.MainPage).Navigation.PushAsync(new View.Mensagem(chat));
+            }
+        }
+
+
+        private List<Chat> _chats;
+        public List<Chat> Chats
+        {
+            get { return _chats; }
+            set
+            {
+                _chats = value;
+                OnPropertyChanged("Chats");
+            }
+        }
+
+        public Command AdicionarCommand { get; set; }
+        public Command OrdenarCommand { get; set; }
+        public Command AtualizarCommand { get; set; }
+
+
+        public ChatsViewModel()
+        {
+            Chats = ServiceWS.GetChats();
+
+            AdicionarCommand = new Command(Adicionar);
+            OrdenarCommand = new Command(Ordenar);
+            AtualizarCommand = new Command(Atualizar);
+        }
+
+        private void Adicionar()
+        {
+            ((NavigationPage)App.Current.MainPage).Navigation.PushAsync(new View.CadastrarChat());
+        }
+
+        private void Ordenar()
+        {
+            Chats = Chats.OrderBy(a => a.nome).ToList();
+        }
+
+        private void Atualizar()
+        {
+            Chats = ServiceWS.GetChats();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string PropertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(PropertyName));
+            }
+        }
+    }
+}
